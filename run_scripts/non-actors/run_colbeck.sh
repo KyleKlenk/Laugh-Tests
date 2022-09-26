0145b7b4c086f9bc28c8d5a3db8d7ba7e75ea670
Kyle <kyle.c.klenk@gmail.com>
2022-09-26 18:37:54 +0000

    cleaned up celia and colbeck files for non-actors version of summa tests

--- run_scripts/non-actors/run_colbeck.sh
+++ run_scripts/non-actors/run_colbeck.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-/SUMMA/bin/summa_sundials.exe -g 1 1 -m /SUMMA/Laugh-Tests/settings/syntheticTestCases/colbeck1976/summa_fileManager_colbeck1976-exp1_test.txt > colbeck1.txt &
-/SUMMA/bin/summa_sundials.exe -g 1 1 -m /SUMMA/Laugh-Tests/settings/syntheticTestCases/colbeck1976/summa_fileManager_colbeck1976-exp2_test.txt > colbeck2.txt &
-/SUMMA/bin/summa_sundials.exe -g 1 1 -m /SUMMA/Laugh-Tests/settings/syntheticTestCases/colbeck1976/summa_fileManager_colbeck1976-exp3_test.txt > colbeck3.txt &
+/SUMMA/bin/summa_sundials.exe -g 1 1 -m /SUMMA/Laugh-Tests/settings/syntheticTestCases/colbeck1976/summa_fileManager_colbeck1976-exp1_test.txt > ../../test_cases/output/non-actors/colbeck1976/colbeck_1.txt
+/SUMMA/bin/summa_sundials.exe -g 1 1 -m /SUMMA/Laugh-Tests/settings/syntheticTestCases/colbeck1976/summa_fileManager_colbeck1976-exp2_test.txt > ../../test_cases/output/non-actors/colbeck1976/colbeck_2.txt
+/SUMMA/bin/summa_sundials.exe -g 1 1 -m /SUMMA/Laugh-Tests/settings/syntheticTestCases/colbeck1976/summa_fileManager_colbeck1976-exp3_test.txt > ../../test_cases/output/non-actors/colbeck1976/colbeck_3.txt
