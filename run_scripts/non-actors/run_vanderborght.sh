ac7b1f063e69f2e87041bf0270ad819f3b81b6a9
KyleKlenk <kyle.c.klenk@gmail.com>
2022-09-27 10:49:12 -0600

    fixed wrong directories for run scripts miller, mizoguchi, vanderborght, and wigmosta

--- run_scripts/non-actors/run_vanderborght.sh
+++ run_scripts/non-actors/run_vanderborght.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-$SUMMADIR/bin/summa_sundials.exe -g 1 1 -m $BASEDIR/settings/syntheticTestCases/vanderborght2005/summa_fileManager_vanderborght2005_exp1_test.txt > ../../test_cases/output/vanderborght2005/vanderborght_1.txt
+$SUMMADIR/bin/summa_sundials.exe -g 1 1 -m $BASEDIR/settings/syntheticTestCases/vanderborght2005/non_actors/summa_fileManager_vanderborght2005_exp1_test.txt > ../../test_cases/output/vanderborght2005/vanderborght_1.txt
 echo "Vanderborght 1 Finished"
 
-$SUMMADIR/bin/summa_sundials.exe -g 1 1 -m $BASEDIR/settings/syntheticTestCases/vanderborght2005/summa_fileManager_vanderborght2005_exp2_test.txt > ../../test_cases/output/vanderborght2005/vanderborght_2.txt
+$SUMMADIR/bin/summa_sundials.exe -g 1 1 -m $BASEDIR/settings/syntheticTestCases/vanderborght2005/non_actors/summa_fileManager_vanderborght2005_exp2_test.txt > ../../test_cases/output/vanderborght2005/vanderborght_2.txt
 echo "Vanderborght 2 Finished"
 
-$SUMMADIR/bin/summa_sundials.exe -g 1 1 -m $BASEDIR/settings/syntheticTestCases/vanderborght2005/summa_fileManager_vanderborght2005_exp3_test.txt > ../../test_cases/output/vanderborght2005/vanderborght_3.txt
+$SUMMADIR/bin/summa_sundials.exe -g 1 1 -m $BASEDIR/settings/syntheticTestCases/vanderborght2005/non_actors/summa_fileManager_vanderborght2005_exp3_test.txt > ../../test_cases/output/vanderborght2005/vanderborght_3.txt
 echo "Vanderborght 3 Finished"
 
 echo "All Vanderborght Tests Finished. Ouptut located in $BASEDIR/test_cases/output/vanderborght2005"
--- run_scripts/non-actors/run_wigmosta.sh
+++ run_scripts/non-actors/run_wigmosta.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-$SUMMADIR/bin/summa_sundials.exe -g 1 1 -m $BASEDIR/settings/syntheticTestCases/wigmosta1999/summa_fileManager-exp1_test.txt > wigmosta1.txt &
+$SUMMADIR/bin/summa_sundials.exe -g 1 1 -m $BASEDIR/settings/syntheticTestCases/wigmosta1999/non_actors/summa_fileManager-exp1_test.txt > wigmosta1.txt &
 echo "wigmosta 1 finished"
 
-$SUMMADIR/bin/summa_sundials.exe -g 1 1 -m $BASEDIR/settings/syntheticTestCases/wigmosta1999/summa_fileManager-exp2_test.txt > wigmosta2.txt &
+$SUMMADIR/bin/summa_sundials.exe -g 1 1 -m $BASEDIR/settings/syntheticTestCases/wigmosta1999/non_actors/summa_fileManager-exp2_test.txt > wigmosta2.txt &
 
 echo "wigmosta 2 finished"
 
