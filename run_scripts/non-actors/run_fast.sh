5bb3a5e6ec7a2760e0327b1666456f438fd752e7
KyleKlenk <kyle.c.klenk@gmail.com>
2022-09-27 11:26:00 -0600

    more path fixes for the non-actors fast tests, miller and vanderborght

--- run_scripts/non-actors/run_fast.sh
+++ run_scripts/non-actors/run_fast.sh
@@ -10,7 +10,7 @@ echo "Starting Colbeck"
 echo ""
 # Miller
 echo "Starting Miller"
-./run_miller.sh
+$SUMMADIR/bin/summa_sundials.exe -g 1 1 -m $BASEDIR/test_cases/settings/syntheticTestCases/miller1998/non_actors/summa_fileManager_millerLoam_test.txt > ../../test_cases/output/non-actors/miller1998/miller_loam.txt
 echo ""
 # Mizoguchi
 echo "Starting Mizoguchi"
